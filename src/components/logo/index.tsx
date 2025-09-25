d009040f98b8862317d3032e3a327c96849c0030
Tusita <tusitaps@gmail.com>
2025-09-25 13:19:23 +0700

    Update index.tsx

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,23 +1,5 @@
-import Link from "next/link";
-import { FaGithub } from "react-icons/fa6";
+// src/components/logo/Logo.tsx
 
-import { useGlobalStore } from "@/stores/global";
-
-// 可替换为你自己的 logo 图片
 export const Logo = () => {
-  const { siteName } = useGlobalStore();
-
-  return (
-    <div className='flex items-center justify-center gap-3 h-16 select-none hover:opacity-80 transition-opacity duration-200'>
-      <Link
-        href='/'
-        className='text-2xl font-bold text-green-600 tracking-tight'
-      >
-        {siteName}
-      </Link>
-      <Link href='https://m.tusita.cyou' target='_blank'>
-        <FaGithub className='w-6 h-6' />
-      </Link>
-    </div>
-  );
+  return null;
 };
